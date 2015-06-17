418cf4b37a7953e4b3656b8e734bc23011404a6e
Anu Mercian <freebirdsanu@gmail.com>
2015-06-17 16:35:20 -0700

    libvirt SSH functionality

--- kernel/src/main/java/net/es/netshell/vm/LibvirtSSHVirtualMachine.java
+++ kernel/src/main/java/net/es/netshell/vm/LibvirtSSHVirtualMachine.java
@@ -8,6 +8,8 @@ import java.lang.StringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.es.netshell.vm.LibvirtVirtualMachine;
+
 /**
  * created by amercian on 06/15/2015
  */
@@ -18,6 +20,8 @@ public class LibvirtSSHVirtualMachine {
      * Functions: SSH into VM, package installations
      */
      private String remoteVMpassword;
+     private String prvKeyFilename;
+
      // to log output
      private static final Logger LOGGER = LoggerFactory.getLogger(LibvirtSSHVirtualMachine.class);
 
@@ -88,8 +92,80 @@ public class LibvirtSSHVirtualMachine {
 	channel.put(String.format("/root/%s",name),String.format("%s",name));
 
 	channel.disconnect();
-	session.disconnect();    
+	session.disconnect(); 
      }
+
+     /**
+      * Exclusive function to delete a session
+      */
+     public void disconnectSession(Session session){
+	session.disconnect();
+     }
+
+     /**
+      * create a session using private key
+      * @param ip IP address
+      * @throws JSchException SSH exception
+      * @throws FileNotFoundException
+      * @throws IOException
+      */
+     public Session createSessionAuth(String ip, String keyPairType, LibvirtVirtualMachine vm) throws JSchException, FileNotFoundException, IOException {
+	JSch jsch=new JSch();
+	//Need to generate this only once
+	KeyPair kpair = generateKeyPair(jsch, keyPairType, vm);
+	jsch.addIdentity(getPrivateKeyFileName());
+	Session session=jsch.getSession("root",ip,22);
+	Properties config=new Properties();
+	config.put("StrictHostKeyChecking","no");
+	session.setConfig(config);
+	//currently disposing key pair to avoid multiple key pairs
+	kpair.dispose();
+	return session;
+     }
+
+     /**
+      * Function to generate a Key Pair
+      * @param keyPairType RSA or DSA 
+      * @param jsch the SSH session
+      * @return KeyPair generated
+      * @throws JSchException KeyGeneration exception
+      * @throws FileNotFoundException for writing key-gen file
+      * @throws IOException 
+      */
+     public KeyPair generateKeyPair(JSch jsch, String keyPairType, LibvirtVirtualMachine vm) throws JSchException, FileNotFoundException, IOException{
+	int type = 0;
+	//Determine how to obtain these
+	String filename = generateFileName(vm.getName(), keyPairType);
+	String comment = "";
+	String passphrase = "";
+	
+	if(keyPairType.equals("rsa")){type=KeyPair.RSA;}
+        else if(keyPairType.equals("dsa")){type=KeyPair.DSA;}
+	else{type=KeyPair.RSA;} //default
+
+	KeyPair kpair=KeyPair.genKeyPair(jsch, type);
+	kpair.setPassphrase(passphrase);
+	kpair.writePrivateKey(filename);
+	setPrivateKeyFileName(filename);
+	kpair.writePublicKey(filename+".pub", comment);
+	System.out.println("Finger print: "+kpair.getFingerPrint());
+	//kpair.dispose();
+	return kpair;
+     }
+
+     private String generateFileName(String name, String keyPairType) {
+	String filename;
+	filename = String.format("~/.ssh/%s/",name) + String.format("id_%s",keyPairType);
+	return filename;
+     }
+
+    private void setPrivateKeyFileName(String prvKeyFilename) {
+	this.prvKeyFilename = prvKeyFilename;
+    }
+
+    private String getPrivateKeyFileName() {
+	return this.prvKeyFilename;
+    }
 
      /**
       * Setting the root password for VM
