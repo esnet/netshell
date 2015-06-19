25b9a9512f6938bb5e00ad99006fca07c7e92af5
Anu Mercian <freebirdsanu@gmail.com>
2015-06-19 10:49:59 -0700

    ssh demo in python api

--- python/src/main/python/libvirt/vmManager.py
+++ python/src/main/python/libvirt/vmManager.py
@@ -1,13 +1,19 @@
 import sys, getopt
 import operator
 import subprocess
+from os.path import expanduser
 from net.es.netshell.vm import LibvirtVirtualMachineDescriptor
 from net.es.netshell.vm import LibvirtManager
 from net.es.netshell.vm import LibvirtVirtualMachine
 from net.es.netshell.vm import VirtualMachineFactory
+from net.es.netshell.vm import LibvirtSSHVirtualMachine
 
 # Authored by amercian
 # on 06/08/2015
+
+#################################################################
+# VM Manager: Modify as per requirements
+#################################################################
 
 #Global variable
 vmName = "" 
@@ -26,7 +32,6 @@ def lxcCreate():
    global name
    global os
    subprocess.call(["sudo", "lxc-create", "-t", "{}".format(os), "-n", "{}".format(name)])
-
 
 def main(argv):
    global name, mem, cpu, container, os, ethName, ip, mac, gateway, netmask
@@ -64,7 +69,7 @@ def main(argv):
 
    print name, mem, cpu, container, os, ethName, ip, mac, gateway, netmask
 
-   #Include Default selections
+   #Default settingss
    if(name == "" or mem == 0 or cpu == 0 or container == "" or os == "" or ethName == "" or ip == "" or mac == "" or gateway == "" or netmask == ""):
       defaultVM()
 
@@ -92,6 +97,27 @@ def defaultVM():
       netmask = "255.255.255.0" #Class A  
    print name, mem, cpu, container, os, ethName, ip, mac, gateway, netmask
 
+def secureShell():
+   ## SSH 
+   ssh = LibvirtSSHVirtualMachine()
+   #SSH with password (not recommended)
+   ssh.setPassword("MYROOTPASS")
+   ssh.commandExecute(ip, "ls -l")
+   #change hostname
+   newname = "test"
+   ssh.commandExecute(ip, "hostname {}".format(newname))
+
+def secureShellKeyGen():
+   #SSH with key-gen pair (recommended)
+   ssh = LibvirtSSHVirtualMachine()
+   ssh.createAuth(ip,"rsa",vm)
+   subprocess.call(["sudo","cp","{}/.ssh/id_rsa.pub".format(expanduser("~")),"/var/lib/lxc/{}/rootfs/root/.ssh/authorized_keys".format(name)]) 
+   session = ssh.createSessionAuth(ip)
+   #Any Processes 
+   ssh.disconnectSession(session)
+   #To enable login to VM using CLI change permissions
+   subprocess.call(["sudo","chmod","a-rwx","{}/.ssh/id_rsa".format(expanduser("~"))])
+   subprocess.call(["sudo","chmod","u+rw","{}/.ssh/id_rsa".format(expanduser("~"))])
 
 if __name__ == "__main__":
    main(sys.argv[1:])
@@ -103,12 +129,14 @@ if __name__ == "__main__":
    cn = LibvirtManager(container) 
    vm = LibvirtVirtualMachine(name, mem, cpu, ethName, ip, gateway, mac, netmask)
 
+   cn.setVirtualMachineFactory(container)
    vm.setName(name)
-   vm.setEthName(ethName)
+   vm.setNetworkName(ethName)
+   vm.setIP(ip)
 
    conn = cn.create(container)
 
-   xml_domain = vm.xmlDomain(name, mem, cpu, container)
+   xml_domain = vm.xmlDomain(name, mem, cpu, cn.getVirtualMachineFactory(), vm.getNetworkName())
 
    xml_network = vm.xmlNetwork(ethName, ip, gateway, mac, netmask)
 
@@ -116,8 +144,13 @@ if __name__ == "__main__":
    net = vm.createNetwork(conn, xml_network)
    dom = vm.create(conn, xml_domain)
 
-   vm.deleteNetwork(conn, net)
+   #TODO LookupDomainName function
 
-   vm.delete(conn, dom)
+   #SSH Capability
+   secureShellKeyGen()
+
+   vm.deleteNetwork(net)
+
+   vm.delete(dom)
 
    cn.delete(conn)
