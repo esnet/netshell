a8fc07a592b0cc24769b7b829f135bc6c1a9d672
Anu Mercian <amercian@lbl.gov>
2015-06-23 17:58:29 -0700

    - fixed a small bug

--- kernel/src/main/java/net/es/netshell/vm/LibvirtVirtualMachine.java
+++ kernel/src/main/java/net/es/netshell/vm/LibvirtVirtualMachine.java
@@ -40,7 +40,7 @@ public class LibvirtVirtualMachine extends LibvirtVirtualMachineDescriptor {
      * @return xml string for Domain
      */
     public String xmlDomain(String name, int memory, int cpu, String virtualMachineFactory, String ethName){
-	String xml_domain = String.format("<domain type='%s'><name>%s</name><memory>%d</memory><os><type>exe</type><init>/sbin/init</init></os><vcpu>%d</vcpu><clock offset='utc'/><on_poweroff>destroy</on_poweroff><on_reboot>restart</on_reboot><on_crash>destroy</on_crash><devices><emulator>/usr/lib/libvirt/libvirt_lxc</emulator><filesystem type='mount'><source dir='/var/lib/lxc/%s/rootfs'/><target dir='/'/></filesystem><interface type='network'><source network='%s'/></interface><console type='pty' /></devices></domain>",virtualMachineFactory, name, memory, cpu, name, ethName);
+	String xml_domain = String.format("<domain type='%s'><name>%s</name><memory>%d</memory><os><type>exe</type><init>/sbin/init</init></os><vcpu>%d</vcpu><clock offset='utc'/><on_poweroff>destroy</on_poweroff><on_reboot>restart</on_reboot><on_crash>destroy</on_crash><devices><emulator>/usr/libexec/libvirt_lxc</emulator><filesystem type='mount'><source dir='/var/lib/libvirt/lxc/%s/'/><target dir='/'/></filesystem><interface type='network'><source network='%s'/></interface><console type='pty' /></devices></domain>",virtualMachineFactory, name, memory, cpu, name, ethName);
 
 	return xml_domain;
     }
@@ -55,7 +55,7 @@ public class LibvirtVirtualMachine extends LibvirtVirtualMachineDescriptor {
      * @return xml string for Network
      */
     public String xmlNetwork(String ethName, String ip, String gateway, String mac, String netmask) {
-	String xml_network = String.format("<network><name>'%s'</name><bridge name='virbr0'/><forward mode='nat'/><ip address='192.168.121.10' netmask='%s'><dhcp><range start='%s' end='%s'/></dhcp></ip></network>",ethName,netmask,ip,ip);
+	String xml_network = String.format("<network><name>%s</name><bridge name='virbr1'/><forward mode='nat'/><ip address='192.168.121.10' netmask='%s'><dhcp><range start='%s' end='%s'/></dhcp></ip></network>",ethName,netmask,ip,ip);
 	return xml_network;
     }
 
@@ -99,7 +99,7 @@ public class LibvirtVirtualMachine extends LibvirtVirtualMachineDescriptor {
     public Network createNetwork(Connect conn, String xml_network) throws LibvirtException{
 	Network net = null;
 	try {
-	    net = conn.networkCreateXML(xml_network);
+	    net = conn.networkDefineXML(xml_network);
 	    net.create();
 	} catch (LibvirtException e) {
             System.out.println("exception caught:"+e);
