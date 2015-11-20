128c81e934b44ab8e1b9ae9671b5a83a12ca3409
Bruce A. Mah <bmah@es.net>
2015-11-20 09:27:26 -0800

    Try to disentangle netshell-odl from netshell-controller so we can get rid of some dead code in netshell-controller.
    netshell-odl is an AD-SAL interface that will be going away anyways, but for now keep it around.

--- odl/src/main/java/net/es/netshell/odl/Controller.java
+++ odl/src/main/java/net/es/netshell/odl/Controller.java
@@ -1,35 +1,34 @@
 /*
- * ENOS, Copyright (c) $today.date, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved.
+ * ENOS, Copyright (c) 2015, The Regents of the University of California,
+ * through Lawrence Berkeley National Laboratory (subject to receipt of any
+ * required approvals from the U.S. Dept. of Energy).  All rights reserved.
  *
- * If you have questions about your rights to use or distribute this software, please contact Berkeley Lab's Technology Transfer Department at TTD@lbl.gov.
+ * If you have questions about your rights to use or distribute this software,
+ * please contact Berkeley Lab's Technology Transfer Department at TTD@lbl.gov.
  *
- * NOTICE.  This software is owned by the U.S. Department of Energy.  As such, the U.S. Government has been granted for itself and others acting on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software to reproduce, prepare derivative works, and perform publicly and display publicly.  Beginning five (5) years after the date permission to assert copyright is obtained from the U.S. Department of Energy, and subject to any subsequent five (5) year renewals, the U.S. Government is granted for itself and others acting on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software to reproduce, prepare derivative works, distribute copies to the public, perform publicly and display publicly, and to permit others to do so.
+ * NOTICE.  This software is owned by the U.S. Department of Energy.  As such,
+ * the U.S. Government has been granted for itself and others acting on its
+ * behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
+ * to reproduce, prepare derivative works, and perform publicly and display
+ * publicly.  Beginning five (5) years after the date permission to assert
+ * copyright is obtained from the U.S. Department of Energy, and subject to
+ * any subsequent five (5) year renewals, the U.S. Government is granted for
+ * itself and others acting on its behalf a paid-up, nonexclusive, irrevocable,
+ * worldwide license in the Software to reproduce, prepare derivative works,
+ * distribute copies to the public, perform publicly and display publicly, and
+ * to permit others to do so.
  */
 
 package net.es.netshell.odl;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import net.es.netshell.controller.OpenFlowNode;
-import net.es.netshell.controller.layer2.Layer2Controller;
-import net.es.netshell.controller.layer2.Layer2ForwardRule;
-import net.es.netshell.controller.layer2.Layer2Port;
-
-import org.apache.commons.codec.binary.Base64;
-import org.opendaylight.controller.sal.action.Action;
-import org.opendaylight.controller.sal.action.Output;
 import org.opendaylight.controller.sal.core.Node;
 import org.opendaylight.controller.sal.core.NodeConnector;
 import org.opendaylight.controller.sal.flowprogrammer.Flow;
 import org.opendaylight.controller.sal.flowprogrammer.IFlowProgrammerService;
-import org.opendaylight.controller.sal.match.MatchType;
 import org.opendaylight.controller.sal.packet.*;
-import org.opendaylight.controller.sal.match.Match;
-import org.opendaylight.controller.sal.action.SetDlDst;
-import org.opendaylight.controller.sal.action.SetDlSrc;
-import org.opendaylight.controller.sal.action.SetVlanId;
 
 import org.opendaylight.controller.sal.utils.Status;
 import org.opendaylight.controller.sal.utils.StatusCode;
@@ -42,7 +41,7 @@ import org.slf4j.LoggerFactory;
  * This class is an interface to the OpenFlow controller functionality in
  * OpenDaylight.
  */
-public class Controller implements Layer2Controller {
+public class Controller {
     // This is a quasi-singleton.  In theory there can be multiple of these objects
     // in a system, but in practice it seems that each one of these is associated
     // with a single instance of the OSGi bundle, which basically just means just
@@ -176,54 +175,4 @@ public class Controller implements Layer2Controller {
         }
         return stat;
     }
-
-
-    private Flow makeFlow(Layer2ForwardRule rule) {
-        Flow flow = null;
-        Match match = new Match();
-        Layer2Port inPort, outPort;
-        inPort = (Layer2Port) rule.getInPort();
-        outPort = (Layer2Port) rule.getOutPort();
-
-        match.setField(MatchType.IN_PORT, inPort.getResourceName());
-        match.setField(MatchType.DL_VLAN,new Short((short) inPort.getVlan()));
-        match.setField(MatchType.DL_DST, rule.inMacToByteArray());
-
-        ArrayList<Action> actions = new ArrayList<Action>();
-        SetVlanId vlanAction = new SetVlanId(new Short((short) outPort.getVlan()));
-        actions.add(vlanAction);
-        SetDlDst dlDstAction = new SetDlDst(rule.outMacToByteArray());
-        actions.add(dlDstAction);
-        Node odlNode = this.findODLSwitch((OpenFlowNode) outPort.getNode()).getNode();
-        Output outputAction = new Output(this.getNodeConnector(odlNode,outPort.getResourceName()));
-        actions.add(outputAction);
-
-        return flow;
-    }
-
-    private Switch findODLSwitch(OpenFlowNode enosSwitch) {
-        List<Switch> switches = this.getNetworkDevices();
-        Switch sw = null;
-
-        // TODO: ODL may still use 6 bytes DPID. To verified.
-        String dpid = enosSwitch.getDpid();
-        for (Switch s : switches) {
-            if (Base64.encodeBase64String(s.getDataLayerAddress()).equals(dpid)) {
-                sw = s;
-                break;
-            }
-        }
-        return sw;
-    }
-
-    @Override
-    public boolean addForwardRule(Layer2ForwardRule rule) {
-        return false;
-    }
-
-    @Override
-    public boolean removeForwardRule(Layer2ForwardRule rule) {
-        return false;
-    }
-
 }
