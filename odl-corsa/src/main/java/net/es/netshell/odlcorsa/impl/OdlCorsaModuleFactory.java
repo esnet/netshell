6a33282f0c25fee34de376b2d55a8e1995fb7057
Bruce A. Mah <bmah@es.net>
2015-12-10 14:01:11 -0800

    Update copyright notices in netshell-odl-corsa to LBL IPO standards.

--- odl-corsa/src/main/java/net/es/netshell/odlcorsa/impl/OdlCorsaModuleFactory.java
+++ odl-corsa/src/main/java/net/es/netshell/odlcorsa/impl/OdlCorsaModuleFactory.java
@@ -1,4 +1,23 @@
 /*
+ * ESnet Network Operating System (ENOS) Copyright (c) 2015, The Regents
+ * of the University of California, through Lawrence Berkeley National
+ * Laboratory (subject to receipt of any required approvals from the
+ * U.S. Dept. of Energy).  All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this
+ * software, please contact Berkeley Lab's Innovation & Partnerships
+ * Office at IPO@lbl.gov.
+ *
+ * NOTICE.  This Software was developed under funding from the
+ * U.S. Department of Energy and the U.S. Government consequently retains
+ * certain rights. As such, the U.S. Government has been granted for
+ * itself and others acting on its behalf a paid-up, nonexclusive,
+ * irrevocable, worldwide license in the Software to reproduce,
+ * distribute copies to the public, prepare derivative works, and perform
+ * publicly and display publicly, and to permit other to do so.
+ */
+
+/*
 * Generated file
 *
 * Generated from: yang module name: netshell-odlcorsa-impl yang module local name: netshell-odlcorsa-impl
