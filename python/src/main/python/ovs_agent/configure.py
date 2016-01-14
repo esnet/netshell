665d53c676924017c02f8eba08b162980cf778a0
sowmya <sowmya@es.net>
2016-01-13 20:02:33 -0800

    Deleting unwanted files

--- python/src/main/python/ovs_agent/configure.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-def main(args):
-    print 'TODO: Implement the following:\ncreate_bridge()\n __attach_primary_interface()\nremove_bridge()\n__detach_primary_interface'
-
-if __name__=='__main__':
-    main(args)
--- python/src/main/python/ovs_agent/container_management.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-def main(args):
-    print 'TODO: Implement the following:\nadd_container_interface()\nremove_container_interface()'
