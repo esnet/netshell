9670d55e092cafea984c0cd2541e7e9161762e9e
lomaxfrog <lomaxfrog@yahoo.com>
2016-04-12 07:30:22 -0700

    - add support for junit 4.         - add skeleton junit test for PersistentObject.java

---
+++ kernel/src/test/java/net/es/netshell/api/PersistentObjectTest.java
@@ -0,0 +1,106 @@
+package net.es.netshell.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lomax on 4/12/16.
+ */
+public class PersistentObjectTest {
+
+    @Test
+    public void testGetResourceClassName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEid() throws Exception {
+
+    }
+
+    @Test
+    public void testSetEid() throws Exception {
+
+    }
+
+    @Test
+    public void testSetResourceClassName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetProperties() throws Exception {
+
+    }
+
+    @Test
+    public void testSetProperties() throws Exception {
+
+    }
+
+    @Test
+    public void testBuildFile() throws Exception {
+
+    }
+
+    @Test
+    public void testToJSON() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveToFile() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+
+    @Test
+    public void testSave1() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveToJSON() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete1() throws Exception {
+
+    }
+
+    @Test
+    public void testNewObjectFromJSON() throws Exception {
+
+    }
+
+    @Test
+    public void testFind() throws Exception {
+
+    }
+
+    @Test
+    public void testFind1() throws Exception {
+
+    }
+
+    @Test
+    public void testNewObjectFromFile() throws Exception {
+
+    }
+
+    @Test
+    public void testNewObjectFromFile1() throws Exception {
+
+    }
+}
