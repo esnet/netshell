e47acfecdfc3ea74ed3489ced98fd775a785a05a
Bruce A. Mah <bmah@es.net>
2016-05-19 09:55:54 -0700

    WIP checkpoint for splitting netshell-controller.
    netshell-controller-intf contains common classes for any code that needs to use the ENOS SDN controller interface.
    
    netshell-odl-provider is the portion of the ENOS SDN controller interface that runs in an ODL container.
    
    netshell-odl-controller is the portion of the ENOS SDN controller interfaces that runs in the ENOS container.
    
    The OSGi feature definition file has been moved to its own OSGi bundle, netshell-features, because that needs to be accessible from both the ODL container and the ENOS container.  features.xml reflects the new intended organization of the various bundles.
    
    No changes to package names at this point.
    
    Doesn't compile yet, because the bundle activator for netshell-odl-provider depends on a class loader for OSGi bundles (to find the optional netshell-odl-corsa driver) that as of now only exists in netshell-kernel, but netshell-odl-provider cannot depend on netshell-kernel.  We should probably see if we can use the OSGi service registry instead (and indeed we do this in the case of the optional netshell-python module), although netshell-odl-corsa's use of MD-SAL complicates matters.

--- odl-provider/src/main/java/net/es/netshell/controller/core/Controller.java
+++ odl-provider/src/main/java/net/es/netshell/controller/core/Controller.java
@@ -20,7 +20,6 @@
 
 package net.es.netshell.controller.core;
 
-import net.es.netshell.boot.BootStrap;
 import net.es.netshell.controller.impl.SdnController;
 import net.es.netshell.odlcorsa.OdlCorsaIntf;
 import net.es.netshell.odlmdsal.impl.OdlMdsalImpl;
@@ -56,6 +55,16 @@ public class Controller {
         return OdlMdsalImpl.getInstance();
     }
 
+    private volatile BundleContext bundleContext;
+
+    public BundleContext getBundleContext() {
+        return bundleContext;
+    }
+
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
     /**
      * Get an object that implements the Corsa glue interface.
      * Note that even loading the code/class for the Corsa glue is optional,
@@ -72,7 +81,7 @@ public class Controller {
                 // This is clunky.  We can't do this everytime we need to invoke this method,
                 // maybe need to cache it or something like that?  The difficulty, as always,
                 // is knowing when to invalidate the cache.
-                BundleContext bc = BootStrap.getBootStrap().getBundleContext();
+                BundleContext bc = this.getBundleContext();
                 Bundle[] bundles = bc.getBundles();
                 OsgiBundlesClassLoader classLoader =
                         new OsgiBundlesClassLoader(bundles, OdlCorsaIntf.class.getClassLoader());
